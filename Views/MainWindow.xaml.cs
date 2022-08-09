68189443016527ef69212eb5c4a65d40437b8ddb
Explosy <moroza.exe@yandex.ru>
2022-08-09 16:33:23 +0300

    Добавьте файлы проекта.

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace RSSFeeder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
