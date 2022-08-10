fafbe4e0441cfee6c967708bc1cbccb890cc528a
Explosy <moroza.exe@yandex.ru>
2022-08-10 14:11:50 +0300

    update Models

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,23 @@
 ﻿using RSSFeeder.Models;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net;
+using System.Xml;
 
 namespace RSSFeeder.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        public ObservableCollection<RssItem> Items { get; }
+        public ObservableCollection<Feed> Feeds { get; }
+        public ObservableCollection<Item> Items { get; }
         
         public MainWindowViewModel()
         {
-            RssFeed rssFeed = new RssFeed("https://habr.com/rss/interesting/");
-            Items = new ObservableCollection<RssItem>(rssFeed.Items);
+            Feeds = new ObservableCollection<Feed>();
+            Feeds.Add(GetFeedByUrl("https://habr.com/rss/interesting/"));
+            Items = new ObservableCollection<Item>(Feeds[0].Items);
+
         }
 
         
@@ -25,6 +31,96 @@ namespace RSSFeeder.ViewModels
                 if (Equals(_Title, value)) return;
                 _Title = value;
                 OnPropertyChanged();
+            }
+        }
+
+        private Feed GetFeedByUrl (string url)
+        {
+            Feed newFeed = new Feed();
+            newFeed.Items = new List<Item>();
+            XmlTextReader xmlTextReader = new XmlTextReader(url);
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            {
+                xmlDoc.Load(xmlTextReader);
+                xmlTextReader.Close();
+                XmlNode channelXmlNode = xmlDoc.GetElementsByTagName("channel")[0];
+
+                if (channelXmlNode != null)
+                {
+                    foreach (XmlNode channelNode in channelXmlNode.ChildNodes)
+                    {
+                        switch (channelNode.Name)
+                        {
+                            case "title":
+                                {
+                                    newFeed.Title = channelNode.InnerText;
+                                    break;
+                                }
+                            case "link":
+                                {
+                                    newFeed.Link = channelNode.InnerText;
+                                    break;
+                                }
+                            case "description":
+                                {
+                                    newFeed.Description = channelNode.InnerText;
+                                    break;
+                                }
+                            case "item":
+                                {
+                                    Item item = new Item();
+                                    foreach (XmlNode xmlTag in channelNode.ChildNodes)
+                                    {
+                                        switch (xmlTag.Name)
+                                        {
+                                            case "title":
+                                                {
+                                                    item.Title = xmlTag.InnerText;
+                                                    break;
+                                                }
+                                            case "link":
+                                                {
+                                                    item.Link = xmlTag.InnerText;
+                                                    break;
+                                                }
+                                            case "description":
+                                                {
+                                                    item.Description = xmlTag.InnerText;
+                                                    break;
+                                                }
+                                            case "pubDate":
+                                                {
+                                                    item.PublishDate = xmlTag.InnerText;
+                                                    break;
+                                                }
+                                        }
+                                    }
+                                    (newFeed.Items as List<Item>).Add(item);
+                                    break;
+                                }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("Ошибка в XML.Описание канала не найдено!");
+                }
+                return newFeed;
+            }
+            catch (WebException exception)
+            {
+                if (exception.Status == WebExceptionStatus.NameResolutionFailure) throw new Exception("Невозможно соединиться с указаным источником.\r\n" + url);
+                else throw exception;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                throw new Exception("Файл" + url + "не найден!");
+            }
+            finally
+            {
+                xmlTextReader.Close();
             }
         }
     }
