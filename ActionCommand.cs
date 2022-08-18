10ca81425c0105fa9daccda3287c0f9b21e4d68f
Explosy <moroza.exe@yandex.ru>
2022-08-18 18:40:30 +0300

    delete unnecessary using

--- ActionCommand.cs
+++ ActionCommand.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace RSSFeeder
 {
@@ -10,7 +7,7 @@ namespace RSSFeeder
     {
         private readonly Action<object> _Execute;
         private readonly Func<object, bool> _CanExecute;
-        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecute = null )
+        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
         {
             _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
             _CanExecute = CanExecute;
